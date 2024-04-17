42b30e4eac8374f389d1e658868eab071d076df7
Borras Quiroga <fborrasquiroga@FRSN.UTN.EDU.AR>
2024-04-17 12:09:52 -0300

    numeropar

---
+++ dias.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    string (Cronograma)
+    int Dias = 0;
+
+    cout << "ingrese un numero del 1 al 7 para saber el dia de la semana" <<endl;
+    cin >> Dias;
+
+    if ()
+
+
+    switch (Dias) {
+
+case 1:
+    cout << "Lunes" << endl;
+    break;
+case 2:
+    cout << "Martes" <<endl;
+    break;
+case 3:
+    cout << "Miercoles" <<endl;
+    break;
+case 4:
+    cout << "Jueves" <<endl;
+    break;
+case 5:
+    cout << "Viernes" <<endl;
+    break;
+case 6:
+    cout << "Sabado" <<endl;
+    break;
+case 7:
+    cout << "Domingo" <<endl;
+    break;
+
+    }
+
+
+
+    return 0;
+}
+
+
+
+
